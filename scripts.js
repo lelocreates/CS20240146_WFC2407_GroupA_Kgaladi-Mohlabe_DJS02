abca1be33a9b4f86e982c6eae87edb6fcccdb6f1
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-10-28 02:06:14 +0200

    add program crash scenario solution

--- scripts.js
+++ scripts.js
@@ -5,11 +5,44 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  const criticalError = document.querySelector('.critical-error');
+  try {
+    if (!dividend || !divider){
+      alert('Division not performed. Both values are required in inputs. Try again')
+    }else if (dividend !== 0 && divider === 0){
+      alert('Division not performed. Invalid number provided. Try again')
+    }else if (isNaN(dividend) || isNaN(divider)){
+      criticalError.style.display = "flex";
+      throw new Error('Something critical went wrong. Please reload the page')
+    } else {
+      const division = dividend / divider
+      const twoDecimals = division.toFixed(2);
+      result.innerText = twoDecimals;
+    }
+  } catch (error) {
+    console.log("Error:", error.message)
+  }
+});
+
+
+
+
+
+
+
+/* form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
   if (!dividend || !divider){
     alert('Division not performed. Both values are required in inputs. Try again')
-  }else{
+  }else if (dividend !== 0 && divider === 0){
+    alert('Division not performed. Invalid number provided. Try again')
+  }else if (isNaN(dividend) && isNaN(divider)){
+    throw new Error('Something critical went wrong. Please reload the page')
+  } else {
     const division = dividend / divider
     const twoDecimals = division.toFixed(2);
     result.innerText = twoDecimals;
   }
-});
+}); */ 
